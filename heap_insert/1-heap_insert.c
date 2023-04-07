2204970638fd296f3366d03fa550b639659af7f7
Facu B <5078@holbertonstudents.com>
2023-04-07 17:39:35 -0300

    Betty

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -10,48 +10,54 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *current;
+    heap_t *new_node, *current;
 
-	new_node = malloc(sizeof(heap_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = value;
-	new_node->parent = NULL;
-	new_node->left = NULL;
-	new_node->right = NULL;
+    new_node = malloc(sizeof(heap_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n = value;
+    new_node->parent = NULL;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-	if (*root == NULL)
-	{
-		*root = new_node;
-		return (new_node);
-	}
-	current = *root;
-	while (current->left != NULL && current->right != NULL)
-	{
-		if (current->left != NULL && current->right != NULL)
-			current = current->left;
-		else if (current->left != NULL)
-			current = current->right;
-	}
+    if (*root == NULL)
+    {
+        *root = new_node;
+        return (new_node);
+    }
+    current = *root;
+    while (current->left != NULL && current->right != NULL)
+    {
+        if (current->left != NULL && current->right != NULL)
+            current = current->left;
+        else if (current->left != NULL)
+            current = current->right;
+    }
 
-	new_node->parent = current;
-	if (current->left == NULL)
-		current->left = new_node;
-	else
-		current->right = new_node;
+    new_node->parent = current;
+    if (current->left == NULL)
+        current->left = new_node;
+    else
+        current->right = new_node;
 
-	while (new_node->parent != NULL && new_node->n > new_node->parent->n)
-	{
-		swap_nodes(new_node->n, new_node->parent->n);
-		new_node = new_node->parent;
-	}
+    while (new_node->parent != NULL && new_node->n > new_node->parent->n)
+    {
+        swap_nodes(new_node->n, new_node->parent->n);
+        new_node = new_node->parent;
+    }
 
-	return (new_node);
+    return (new_node);
 }
+
+/**
+ * swap_nodes - Swap values in the nodes
+ * @n1: node value1
+ * @n2: node value2
+ */
 void swap_nodes(int n1, int n2)
 {
-	int temp = n1;
+    int temp = n1;
 
-	n1 = n2;
-	n2 = temp;
+    n1 = n2;
+    n2 = temp;
 }
