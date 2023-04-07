e895da0ad3fa6bc5e410ba60f789df1d0f87f2cb
Facu B <5078@holbertonstudents.com>
2023-04-07 16:01:56 -0300

    betty

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -8,19 +8,18 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = NULL;
+	binary_tree_t *newnode = NULL;
 
-    newnode = malloc(sizeof(binary_tree_t));
-    if (!newnode)
-        return (NULL);
+	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
+		return (NULL);
 
-    newnode->n = value;
-    newnode->left = newnode->right = NULL;
+	newnode->n = value;
+	newnode->left = newnode->right = NULL;
+	if (!parent)
+		newnode->parent = NULL;
+	else
+		newnode->parent = parent;
 
-    if (!parent)
-        newnode->parent = NULL;
-    else
-        newnode->parent = parent;
-
-    return (newnode);
+	return (newnode);
 }
